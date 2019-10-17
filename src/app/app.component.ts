f303bc30e28d44184317fa196365d5a8bcbd5866
Yuda Ferry Mahendra <juncyboy@gmail.com>
2019-10-17 09:55:52 +0700

    just simple layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'simpleAngularCalculator';
+  temp:string = '';
+
+  addToTemp(val) {
+    this.temp = this.temp + val;
+  }
 }
